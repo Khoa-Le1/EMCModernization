14b340204f169adc9d67114a76ea37ded41417ef
Regie <regilyn.feliciano@gmail.com>
2018-01-24 14:15:53 -0600

    partial templating

---
+++ src/main/java/ca/ehealthsask/emc/demo/HomeController.java
@@ -0,0 +1,56 @@
+package ca.ehealthsask.emc.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.FileOutputStream;
+
+/**
+ * Partially for testing purposes
+ *
+ */
+@Controller
+public class HomeController {
+
+    private static final String appName = "EMC";
+
+    @GetMapping("/")
+    public String home(Model model,
+                       @RequestParam(value = "name", required = false,
+                               defaultValue = "Guest") String name) {
+
+        model.addAttribute("name", appName);
+        model.addAttribute("title", appName);
+        return "index";
+    }
+
+
+
+    @GetMapping("/remediation/remediation_queue.html")
+    public String remediationQueue(Model model,
+                       @RequestParam(value = "name", required = false,
+                               defaultValue = "Guest") String name) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("title", appName);
+        return "remediation/remediation_queue";
+    }
+
+    /**Testing
+     *
+     * @param model
+     * @param name
+     * @return
+     */
+    @GetMapping("/demo.html")
+    public String demo(Model model,
+                       @RequestParam(value = "name", required = false,
+                               defaultValue = "Guest") String name) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("title", appName);
+        return "remediation/demo";
+    }
+}
