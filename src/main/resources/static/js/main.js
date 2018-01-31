2f0da5bc680bb384454e74cfb223abf3d1d6c189
Khoa <khoalittlesaigon@hotmail.com>
2018-01-31 04:09:14 -0600

    Update 2.1 HTML edits and CSS edits

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -157,74 +157,48 @@ function msgError(msg,duration) {
 }
 
 function switchForm(){
-    /*
-    var docSetID = "  <div class=\"col s7 input-field\" id=\"docSetID\">\n" +
-        "                    <label>Document Set ID</label>\n" +
-        "                    <input type=\"text\" name=\"document-setID\" url-updater>\n" +
-        "                </div>";
-    var orderNumber = "  <div class=\"col s7 input-field\" id=\"orderNumber\">\n" +
-        "                    <label>Order Number</label>\n" +
-        "                    <input type=\"text\" name=\"order-number\" url-updater>\n" +
-        "                </div>";
-    var visitNum = "  <div class=\"col s7 input-field\" id=\"visitNumber\">\n" +
-        "                    <label>Visit Number</label>\n" +
-        "                    <input type=\"text\" name=\"visit-number\" url-updater>\n" +
-        "                </div>";
-    var docID = "  <div class=\"col s7 input-field\" id=\"docID\">\n" +
-        "                    <label>Document ID</label>\n" +
-        "                    <input type=\"text\" name=\"doc-ID\" url-updater>\n" +
-        "                </div>";
-        */
-    //var source = "";
     if ($("#domain").val() == "CDM"){
-        $("#source").html("<div id=\"source\">\n" +
-            "            </div>");
+        $("#formType").html("<h5 class=\"title\">Chronic Disease Management Message Inquiry</h5>");
+        $("#source").hide();
         $("#element1").html("  <div class=\"col s7 input-field\" id=\"docSetID\">\n" +
             "                    <label>Document Set ID</label>\n" +
             "                    <input type=\"text\" name=\"document-setID\" url-updater>\n" +
             "                </div>");
     }
     else if($("#domain").val() == "CD"){
-        $("#source").html("<div id=\"source\">\n" +
-            "            </div>");
+        $("#formType").html("<h5 class=\"title\">Clinical Document Message Inquiry</h5>");
+        $("#source").hide();
         $("#element1").html("  <div class=\"col s7 input-field\" id=\"docID\">\n" +
             "                    <label>Document ID</label>\n" +
             "                    <input type=\"text\" name=\"doc-ID\" url-updater>\n" +
             "                </div>");
     }
     else if($("#domain").val() == "CE"){
-        $("#source").html("<div id=\"source\">\n" +
-            "            </div>");
+        $("#formType").html("<h5 class=\"title\">Clinical Encounter Message Inquiry</h5>");
+        $("#source").hide();
         $("#element1").html("  <div class=\"col s7 input-field\" id=\"visitNumber\">\n" +
             "                    <label>Visit Number</label>\n" +
             "                    <input type=\"text\" name=\"visit-number\" url-updater>\n" +
             "                </div>");
     }
     else if($("#domain").val() == "LAB"){
-        $("#source").html("<div id=\"source\">\n" +
-            "            </div>");
+        $("#formType").html("<h5 class=\"title\">Lab Message Inquiry</h5>");
+        $("#source").hide();
         $("#element1").html("  <div class=\"col s7 input-field\" id=\"orderNumber\">\n" +
             "                    <label>Order Number</label>\n" +
             "                    <input type=\"text\" name=\"order-number\" url-updater>\n" +
             "                </div>");
     }
     else if($("#domain").val() == "MI"){
-        $("#source").html("<div class=\"col s7 input-field\" id=\"source\">\n" +
-            "                <select name=\"sourceSelect\" id=\"sourceSelect\">\n" +
-            "                                    <option value=\\\"CBR-MAYFAIR\\\">CBR-MAYFAIR</option>\n" +
-            "                                    <option value=\\\"CBR-RAR\\\">CBR-RAR</option>\n" +
-            "                                    <option value=\\\"RIS_HNAM\\\">RIS_HNAM</option>\n" +
-            "                                    <option value=\\\"RIS_XIRIS\\\">RIS_XIRIS</option>\n" +
-            "                                </select>\n" +
-            "                            <label>Source</label>\n" +
-            "            </div>");
+        $("#formType").html("<h5 class=\"title\">Medical Imaging Reports Message Inquiry</h5>");
+        $("#source").show();
         $("#element1").html("  <div class=\"col s7 input-field\" id=\"docID\">\n" +
             "                    <label>Document ID</label>\n" +
             "                    <input type=\"text\" name=\"doc-ID\" url-updater>\n" +
             "                </div>");
 
     }else{
-
+        $("#source").hide();
     }
 }
 //EXTRA FUNCTIONALITY
